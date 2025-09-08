382762216ce60bb168237965027d1214d205e18d
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-09-08 13:17:23 +0200

    Copy QMD from nbis_retreat_presentation_20250313

---
+++ add_border.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+convert home.png -bordercolor black -border 1x1 home_with_border.png
+convert bianca_main_page.png -bordercolor black -border 1x1 bianca_main_page_with_border.png
+convert rt.png -bordercolor black -border 1x1 rt_with_border.png
+convert winscp.png -bordercolor black -border 1x1 winscp_with_border.png
+
+
